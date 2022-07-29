81fbc712804ceedfa6c2f9cdee6a72e271636656
Jake Wehder <jw@email.comjake>
2022-07-28 19:40:30 -0500

    commit tree 10bb51de924582c280d6c9eb73b9a467fa6d6e58

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const record = [
+    {year: '2015', result:'W'},
+    {year: '2014', result: 'N/A'},
+    {year: '2013', result: 'L'},
+    //....
+]
+
+const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+
+function superbowlWin(arr) {
+    let win = arr.find(item => item.result === 'W')
+    if (typeof win === 'undefined'){
+        return win
+    }
+    return win['year']
+}   
+
+console.log(superbowlWin(sadReality))
+console.log(superbowlWin(record))
